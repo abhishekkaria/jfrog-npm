16f58c7138f4f4fe6a40415a8a331ed8614c29ca
Abhishek <abhishek.karia.007@gmail.com>
2024-10-30 15:35:29 +0530

    env in yml

---
+++ src/MyButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const MyButton = ({ label, onClick }) => {
+    return React.createElement(
+        'button',
+        { onClick: onClick, style: styles.button },
+        label
+    );
+};
+
+const styles = {
+    button: {
+        padding: '10px 20px',
+        backgroundColor: '#007BFF',
+        color: 'white',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        fontSize: '16px',
+    },
+};
+
+export default MyButton;
