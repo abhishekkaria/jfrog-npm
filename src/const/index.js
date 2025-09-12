a982ad537214278ee24312226c40fd034daea60c
Abhishek Karia <abhishek.karia.007@gmail.comm>
2025-09-12 20:51:36 +0530

    SonarQube duplication test

---
+++ src/const/index.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: 'Wireless Mouse', price: 29.99, inStock: true },
+  { id: 2, name: 'Bluetooth Keyboard', price: 49.99, inStock: false },
+  { id: 3, name: 'HD Monitor', price: 199.99, inStock: true },
+  { id: 4, name: 'USB-C Hub', price: 24.99, inStock: true },
+  { id: 5, name: 'Webcam', price: 89.99, inStock: false }
+];
